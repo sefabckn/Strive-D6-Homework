0e0a6467df5931eaab0d87560a63c521edaf3057
sefabckn <sefabckn@gmail.com>
2021-09-27 10:20:05 +0200

    Exercise E solved

--- D6-Homework.js
+++ D6-Homework.js
@@ -42,7 +42,8 @@ let me = {
 /* EXERCISE E
     Write a piece of code for programmatically removing the age property from the previously create object.
 */
-
+delete me.age;
+console.log(me)
 /* EXERCISE F
    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
 */
